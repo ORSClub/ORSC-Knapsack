cf25aca4e734bce90b8a88e32b5b3eb2bd9f97a9
Abdelmalik Benfadhil <malikbf5@gmail.com>
2023-03-12 11:32:32 +0000

    Update knapsackstreamlit.py

--- knapsackstreamlit.py
+++ knapsackstreamlit.py
@@ -112,6 +112,10 @@ Branch & Bound is an approach based on the principle that the total set of feasi
 
 st.write(""" It is important to note that while this problem seems simple, it becomes exponentially more complex when the number of objects(**n**) goes higher, which makes the knapsack problem an **NP** complete problem""")
 
-st.write(""" Knapsack problems appear in real-world decision-making processes in a wide variety of fields, such as finding the least wasteful way to cut raw materials, selection of investments and portfolios, selection of assets for asset-backed securitization, and generating keys for the Merkle–Hellman[6] and other knapsack cryptosystems.""")
+st.write(""" Knapsack problems appear in real-world decision-making processes in a wide variety of fields, such as: 
+- finding the least wasteful way to cut raw materials 
+- selection of investments and portfolios, selection of assets for asset-backed securitization 
+- container ship and aircraft loading 
+- generating keys for the Merkle–Hellman and other knapsack cryptosystems""")
 st.image(img)
 
